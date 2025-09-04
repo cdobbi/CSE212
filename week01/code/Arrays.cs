41e5fe317bed0aece33287f5a8ec97c7965328ef
Christina Dobbins <u800176908@byui.edu>
2025-09-04 14:09:48 -0600

    Part 2: Solving a Complicated Problem Using a List

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,11 +1,12 @@
+// Christina Dobbins CSE212 W01 Code: Dynamic Arrays Running and Debugging
 // 9-4-25 10:49 AM
 
-//Christina Dobbins CSE212 W01 Code: Dynamic Arrays Running and Debugging
 // Part 1: Arrays
-// I will read over the file and understand the requirements for each function before implementing the code.
-// In order to understand the requirements fully, I will break down the tasks for each problem.
-// For the tasks I'm not understanding I'll consult the documentation, seek help from others to clarify what's expected and to teach me or ask GPT.
-// When I ask others or GPT to teach me, I will be specific about what I want to learn, and specify not to give me the answers, to help me learn.
+// Read over the file and understand the requirements for each function before implementing code.
+// In order to understand the requirements fully, break down the tasks for each problem.
+// If not understanding, consult the module documentation, get help from others to clarify, ask GPT what's expected and to teach me.
+// When asking others or GPT to teach, be specific about the missunderstanding, specify not to give the answers, to only teach.
+
 
 public static class Arrays
 {
@@ -13,22 +14,44 @@ public static class Arrays
     /// Come up with a plan on how to implement the MultiplesOf function.
     /// The function should create and return an array of multiples of a number.
     /// The starting number and the number of multiples are provided as inputs to the function.
-    /// For example, MultiplesOf(3,5), where the 3 is the starting number and 5 is the number of multiples, would result in <double>{3, 6, 9, 12, 15}.
     /// This function will produce an array of size 'length' starting with 'number' followed by multiples of 'number'.
     /// For example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.
     /// Assume that length is a positive integer greater than 0.
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
+    // public: anyone can use this function
+    // static: belongs to the class, not an object
+    // double[ ]: returns an array of decimal numbers
+    // MultiplesOf: function name
+    // (double number, int length): parameters takes a decimal number and a whole number as inputs.
     {
-        // Part 1: Arrays 9-4-25 10:56 AM
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+      // Step 1: Create an array to hold the multiples.
+        double[] multiples = new double[length];
+        // double[]: make an array of decimal numbers
+        // multiples: name of teh array
+        // = new double[length];: create the array with 'length' spots
 
-        return []; // replace this return statement with your own
+        // Step 2: Use a loop to to the math for them.
+        for (int i = 0; i < count; i++)
+        // for: start a loop
+        // int i = 0: start with i at zero
+        // i < length: keep going while i is less than length
+        // i++: add one to i each time
+
+        // Step 3: Store them to an array.
+            multiples[i] = number * (i + 1);
+            // multiples[i]: the spot in the array at the position i.
+            // =: set it equal to....
+            // number * (i +1): the starting number times (i +1)
+            // (i +1): makes the multiples start at 1 (not 0).
+
+        // Step 4: Return the array.
+        return multiples;
+        // return: give back
+        // multiples: the array filled with answers
     }
+
 
     /// <summary>
     /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
@@ -37,18 +60,24 @@ public static class Arrays
     ///
     /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
     /// </summary>
+
+
     public static void RotateListRight(List<int> data, int amount)
-    {
-        // 
-        // Part 2: Solving a Complicated Problem Using a List
-        // Come up with a plan on how to implement the RotateListRight function.
-        // This function receives a list of data and an amount to rotate to the right.
-        // For example, if the data is <List>{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 5 then the list after the function runs should be <List>{ 5, 6, 7, 8, 9, 1, 2, 3, 4}.
-        // If the data is <List>{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 3 then the list after the function runs should be <List>{7, 8, 9, 1, 2, 3, 4, 5, 6}. The value of amount will be in the range of 1 and data.Count, inclusive.
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
-    }
+    {        
+       // Step 1: Get the last 'amount' elements.
+       List<int> endElements = data.GetRange(data.Count - amount, amount);
+       // List<int>: make a new list of intergers.
+       // endElements: name of the new list
+       // = data.GetRange(...): get a section of the original list.
+       // data.Count - amount: start at the right spot
+       // amount: how many elements to get
+
+       // Step 2: Remove thos elements fromt the end.
+       data.RemoveRange(data.Count - amount, amount);
+       // data.RemoveRange(...): remove a section from the list.
+
+       // Step 3: Insert those elements at the front.
+       data.InsertRange(0, endElements);
+       // data.InsertRange(0, ...): put the elements at the start of the list.
 }
 
